071167b9f547588ec4892a679052252c3d5862ee
e <nuc_baixu@163.com>
2022-01-28 15:30:12 +0800

    change some example

--- examples/python/geometry/camera_trajectory.py
+++ examples/python/geometry/camera_trajectory.py
@@ -1,35 +1,21 @@
-# ----------------------------------------------------------------------------
-# -                        Open3D: www.open3d.org                            -
-# ----------------------------------------------------------------------------
-# The MIT License (MIT)
-#
-# Copyright (c) 2018-2021 www.open3d.org
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-# ----------------------------------------------------------------------------
+#深度图重建点云算法原理
+#blog:https://zhuanlan.zhihu.com/p/34645454
 
 # examples/python/geometry/camera_trajectory.py
 
 import numpy as np
 import open3d as o3d
+import sys
+import os
+
+# only needed for tutorial, monkey patches visualization
+sys.path.append('..')
 
 if __name__ == "__main__":
+
+    
+
+    print("当前路径 -> %s" %os.getcwd())
 
     print("Testing camera in open3d ...")
     intrinsic = o3d.camera.PinholeCameraIntrinsic(
@@ -46,22 +32,23 @@ if __name__ == "__main__":
 
     print("Read a trajectory and combine all the RGB-D images.")
     pcds = []
-    trajectory = o3d.io.read_pinhole_camera_trajectory(
-        "../../test_data/RGBD/trajectory.log")
+    trajectory_path = '../test_data/RGBD/trajectory.log'
+    trajectory = o3d.io.read_pinhole_camera_trajectory(trajectory_path)
     o3d.io.write_pinhole_camera_trajectory("test.json", trajectory)
     print(trajectory)
     print(trajectory.parameters[0].extrinsic)
     print(np.asarray(trajectory.parameters[0].extrinsic))
     for i in range(5):
         im1 = o3d.io.read_image(
-            "../../test_data/RGBD/depth/{:05d}.png".format(i))
+            "../test_data/RGBD/depth/{:05d}.png".format(i))
         im2 = o3d.io.read_image(
-            "../../test_data/RGBD/color/{:05d}.jpg".format(i))
+            "../test_data/RGBD/color/{:05d}.jpg".format(i))
         im = o3d.geometry.RGBDImage.create_from_color_and_depth(
             im2, im1, 1000.0, 5.0, False)
         pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
             im, trajectory.parameters[i].intrinsic,
             trajectory.parameters[i].extrinsic)
+       
         pcds.append(pcd)
     o3d.visualization.draw_geometries(pcds)
     print("")
